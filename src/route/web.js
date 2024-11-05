0a3a6efb8b96d798c97bda6da31e6dac34307d03
PhungHuuTai <phunghuutai7@gmail.com>
2024-11-05 21:08:13 +0700

    api get all users or a user

--- src/route/web.js
+++ src/route/web.js
@@ -17,6 +17,7 @@ let initWebRoutes = (app) => {
 
 
     router.post('/api/login', userController.handleLogin);
+    router.get('/api/get-all-user', userController.handleGetAllUsers);
 
     return app.use("/", router);
 }
