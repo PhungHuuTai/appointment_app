1ba54a6ea8b60e3cb2f3aa1844fe3c3ca0b7cf43
PhungHuuTai <phunghuutai7@gmail.com>
2024-11-26 11:35:13 +0700

    Customize header admin and multilingual display

--- src/containers/App.js
+++ src/containers/App.js
@@ -11,7 +11,6 @@ import { userIsAuthenticated, userIsNotAuthenticated } from '../hoc/authenticati
 import { path } from '../utils'
 
 import Home from '../routes/Home';
-// import Login from '../routes/Login';
 import Login from './Auth/Login'
 import Header from './Header/Header';
 import System from '../routes/System';
@@ -45,7 +44,6 @@ class App extends Component {
             <Fragment>
                 <Router history={history}>
                     <div className="main-container">
-                        {this.props.isLoggedIn && <Header />}
                         <div className="content-container">
                             <CustomScrollbars style={{ height: '100vh', width: '100%' }}>
                                 <Switch>
@@ -73,7 +71,6 @@ class App extends Component {
 const mapStateToProps = state => {
     return {
         started: state.app.started,
-        isLoggedIn: state.user.isLoggedIn
     };
 };
 
